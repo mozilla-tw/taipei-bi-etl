99ca6a3d57145fc069e290aa48dca7873bfd9b7b
Eddie Lin <yshlin@gmail.com>
2019-08-17 17:00:48 +0800

    Added args and settings for dest/steps, initial check-in load_to_gcs.

--- tasks/base.py
+++ tasks/base.py
@@ -5,6 +5,10 @@ import requests
 import datetime
 import pandas_gbq as pdbq
 import pandas as pd
+from google.cloud import storage
+
+
+DEFAULT_DATE_FORMAT = '%Y-%m-%d'
 
 
 def get_arg_parser():
@@ -15,13 +19,25 @@ def get_arg_parser():
         help="The ETL task to run.",
     )
     parser.add_argument(
-        "--data-source",
+        "--source",
         default=None,
-        help="The ETL data source to extract.",
+        help="The ETL data source to extract, use the name specified in settings.",
+    )
+    parser.add_argument(
+        "--dest",
+        default=None,
+        help="The place to load transformed data to, can be 'fs' or 'gcs'.\n"
+             "Default is 'gcs', which the intermediate output will still write to 'fs'",
+    )
+    parser.add_argument(
+        "--step",
+        default=None,
+        help="The ETL step to run to, can be 'extract', 'transform', 'load', or just the first letter. \n"
+             "Default is 'load', which means go through the whole ETL process.",
     )
     parser.add_argument(
         "--date",
-        type=lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'),
+        type=lambda x: datetime.datetime.strptime(x, DEFAULT_DATE_FORMAT),
         default=datetime.datetime.today(),
         help="The base (latest) date of the data in YYYY-MM-DD format.",
     )
@@ -42,40 +58,53 @@ def get_arg_parser():
 
 class EtlTask:
 
-    def __init__(self, args, dependencies):
+    def __init__(self, args, sources, destinations, stage):
+        self.stage = stage
         self.args = args
         self.period = args.period
         self.current_date = args.date
         self.last_month = args.date
         self.init_dates()
-        self.dependencies = dependencies
+        self.sources = sources
+        self.destinations = destinations
         self.extracted = dict()
+        self.transformed = dict()
+        self.gcs = storage.Client()
 
     def init_dates(self):
         self.last_month = self.current_date - datetime.timedelta(days=self.period)
 
-    def is_cached(self, dependency, config):
-        fpath = './data/{}_latest.json'.format(dependency)
+    def get_filepath(self, source, config, stage, dest):
+        return '{}{}'.format(self.destinations[dest]['prefix'], self.get_filename(source, config, stage))
+
+    def get_filename(self, source, config, stage):
+        ftype = 'json' if 'load_type' not in config else config['load_type']
+        return '{stage}-{task}-{source}-{date}.{ext}'.format(
+            stage=stage, task=self.args.task, source=source,
+            date=self.current_date.strftime(DEFAULT_DATE_FORMAT), ext=ftype)
+
+    def is_cached(self, source, config):
+        fpath = self.get_filepath(source, config, 'raw', 'fs')
         return os.path.isfile(fpath)
 
-    def extract_via_filesystem(self, dependency, config):
-        fpath = './data/{}_latest.json'.format(dependency)
+    def extract_via_fs(self, source, config, stage='raw'):
+        fpath = self.get_filepath(source, config, stage, 'fs')
         with open(fpath, 'r') as f:
             extracted = f.read()
-        print('{} exists, extracted from filesystem'.format(fpath))
+        print('%s extracted from file system' % source)
         return extracted
 
-    def extract_via_api(self, dependency, config):
+    def extract_via_api(self, source, config):
 
         url = config['url'].format(api_key=config['api_key'],
                                    start_date=self.last_month.strftime(config['date_format']),
                                    end_date=self.current_date.strftime(config['date_format']))
         r = requests.get(url, allow_redirects=True)
         extracted = r.content
-
+        print('%s extracted from API' % source)
         return extracted
 
-    def extract_via_bq(self, dependency, config):
+    def extract_via_bq(self, source, config):
         query = ''
         if 'udf' in config:
             for udf in config['udf']:
@@ -89,35 +118,60 @@ class EtlTask:
             with open('sql/{}.sql'.format(config['query'])) as f:
                 query += f.read().format(from_date=self.last_month.strftime(config['date_format']))
         df = pdbq.read_gbq(query)
+        print('%s extracted from BigQuery' % source)
         return df
 
     def extract(self):
-        for dependency in self.dependencies:
-            if not self.args.data_source or self.args.data_source == dependency:
-                if self.dependencies[dependency]['type'] == 'api':
-                    config = self.dependencies[dependency]
+        for source in self.sources:
+            if not self.args.source or self.args.source == source:
+                if self.sources[source]['type'] == 'api':
+                    config = self.sources[source]
                     if 'cache_file' in config and config['cache_file']:
-                        if not self.is_cached(dependency, config):
-                            self.extracted[dependency] = self.extract_via_api(dependency, config)
-                            self.load_to_filesystem(dependency, config)
+                        if not self.is_cached(source, config):
+                            self.extracted[source] = self.extract_via_api(source, config)
+                            self.load_to_fs(source, config)
+                            self.load_to_gcs(source, config)    # copy to GCS
                         else:
-                            self.extracted[dependency] = self.extract_via_filesystem(dependency, config)
+                            self.extracted[source] = self.extract_via_fs(source, config)
                     else:
-                        self.extracted[dependency] = self.extract_via_api(dependency, config)
-                elif self.dependencies[dependency]['type'] == 'bq':
-                    self.extracted[dependency] = self.extract_via_bq(dependency, self.dependencies[dependency])
+                        self.extracted[source] = self.extract_via_api(source, config)
+                elif self.sources[source]['type'] == 'bq':
+                    self.extracted[source] = self.extract_via_bq(source, self.sources[source])
 
     def transform(self):
-        print('Transform data here.')
+        for source in self.sources:
+            if not self.args.source or self.args.source == source:
+                config = self.sources[source]
+                transform_method = getattr(self, 'transform_{}'.format(source))
+                self.transformed[source] = transform_method(source, config)
+                print('%s transformed' % source)
 
-    def load_to_filesystem(self, dependency, config):
-        fpath = './data/{}_latest.json'.format(dependency)
+    def load_to_fs(self, source, config, stage='raw'):
+        fpath = self.get_filepath(source, config, stage, 'fs')
         with open(fpath, 'wb') as f:
-            f.write(self.extracted[dependency])
-            print('{} saved'.format(fpath))
+            f.write(self.extracted[source])
+            print('%s loaded to file system.' % source)
 
-    def load_to_gcs(self):
-        print('Load data to GCS here.')
+    def load_to_gcs(self, source, config, stage='raw'):
+        bucket = self.gcs.get_bucket(self.destinations['gcs']['bucket'])    # FIXME: run into permimssion problem, may need to specify auth token explicitly
+        blob = bucket.blob(self.get_filepath(source, config, stage, 'gcs'))
+        blob.upload_from_filename(self.get_filepath(source, config, stage, 'fs'))
+        print('%s loaded to GCS.' % source)
 
     def load(self):
-        self.load_to_gcs()
+        for source in self.sources:
+            if not self.args.source or self.args.source == source:
+                config = self.sources[source]
+                self.load_to_fs(source, config, self.stage)
+                if self.args.dest is not 'fs':
+                    self.load_to_gcs(source, config, self.stage)
+
+    def run(self):
+        if self.args.step and self.args.step[0].upper() not in ['E', 'T', 'L']:
+            raise ValueError('Invalid argument specified.')
+        if not self.args.step or self.args.step[0].upper() in ['E', 'T', 'L']:
+            self.extract()
+        if not self.args.step or self.args.step[0].upper() in ['T', 'L']:
+            self.transform()
+        if not self.args.step or self.args.step[0].upper() in ['L']:
+            self.load()
--- tasks/revenue.py
+++ tasks/revenue.py
@@ -4,6 +4,9 @@ import settings
 
 class RevenueEtlTask(base.EtlTask):
 
+    def __init__(self, args, sources, destinations):
+        super().__init__(args, sources, destinations, 'staging')    # define the stage of final output here
+
     def transform(self):
         super().transform()
         print('Transform revenue data here')
@@ -13,8 +16,8 @@ arg_parser = base.get_arg_parser()
 
 
 def main(args):
-    task = RevenueEtlTask(args, settings.SOURCES)
-    task.extract()
+    task = RevenueEtlTask(args, settings.SOURCES, settings.DESTINATIONS)
+    task.run()
 
 
 if __name__ == "__main__":
