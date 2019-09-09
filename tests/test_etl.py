7125683f54e0d2f83c6fd2766050340671408206
Eddie Lin <elin@mozilla.com>
2019-09-09 08:04:55 +0800

    Fixed source check in rps extract.

--- tests/test_etl.py
+++ tests/test_etl.py
@@ -9,3 +9,9 @@ import etl
 def test_etl():
     sys.argv = ["./etl.py", "--debug"]
     etl.main()
+
+@pytest.mark.unittest
+def test_rps_global_package_fs():
+    sys.argv = ["./etl.py", "--debug", "--config=test", "--task=rps", "--step=e", "--source=global_package", "--dest=fs"]
+    etl.main()
+
