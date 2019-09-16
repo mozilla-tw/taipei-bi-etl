40ffa77e8d07a8e4ec2f1ce5c3a1e0004e6ad847
Eddie Lin <elin@mozilla.com>
2019-09-16 14:31:48 +0800

    Use get_config for test config.

--- tests/tasks/test_revenue.py
+++ tests/tasks/test_revenue.py
@@ -8,53 +8,15 @@ import numpy as np
 import utils.common
 from google.cloud import storage
 from google.cloud.storage import Bucket
-import utils.config
 from pandas import DataFrame
 from tasks import base, revenue
 from tests.utils import inject_fixtures
 from argparse import Namespace
+from utils.config import get_configs
 
 log = logging.getLogger(__name__)
 
-SOURCES = {
-    "google_search": {
-        "type": "bq",
-        "project": "moz-fx-prod",
-        "dataset": "telemetry",
-        "table": "focus_event",
-        "udf_js": ["json_extract_events"],
-        "query": "revenue_search_events",
-        "load": True,
-        "date_format": "%Y-%m-%d",
-    },
-}
-SCHEMA = [
-    ("source", np.dtype(object).type),
-    ("country", np.dtype(object).type),
-    ("os", np.dtype(object).type),
-    ("utc_datetime", np.datetime64),
-    ("tz", np.dtype(object).type),
-    ("currency", np.dtype(object).type),
-    ("sales_amount", np.dtype(float).type),
-    ("payout", np.dtype(float).type),
-    ("fx_defined1", np.dtype(object).type),
-    ("fx_defined2", np.dtype(object).type),
-    ("fx_defined3", np.dtype(object).type),
-    ("fx_defined4", np.dtype(object).type),
-    ("fx_defined5", np.dtype(object).type),
-    ("conversion_status", np.dtype(object).type),
-]
-DESTINATIONS = {
-    "gcs": {
-        "bucket": "moz-fx-data",
-        "prefix": "taipei/",
-    },
-    "fs": {
-        "prefix": "test-data/",
-        "file_format": "jsonl",
-        "date_field": "utc_datetime",
-    }
-}
+cfg = get_configs("revenue", "test")
 
 task = "revenue"
 inject_fixtures(
@@ -112,8 +74,8 @@ def test_revenue_google_search_extract_via_bq(mock_pdbq):
                      source='google_search',
                      step='e',
                      task='revenue')
-    task = revenue.RevenueEtlTask(args, SOURCES, SCHEMA, DESTINATIONS)
-    df = task.extract_via_bq("google_search", SOURCES["google_search"])
+    task = revenue.RevenueEtlTask(args, cfg.SOURCES, cfg.SCHEMA, cfg.DESTINATIONS)
+    df = task.extract_via_bq("google_search", cfg.SOURCES["google_search"])
     assert isinstance(df, DataFrame)
     assert df.equals(queryResult)
 
@@ -134,7 +96,7 @@ def test_revenue_google_search_extract(mock_pdbq):
                      source='google_search',
                      step='e',
                      task='revenue')
-    task = revenue.RevenueEtlTask(args, SOURCES, SCHEMA, DESTINATIONS)
+    task = revenue.RevenueEtlTask(args, cfg.SOURCES, cfg.SCHEMA, cfg.DESTINATIONS)
     task.extract()
     df = task.extracted[args.source]  # pylint: disable=no-member
     assert isinstance(df, DataFrame)
