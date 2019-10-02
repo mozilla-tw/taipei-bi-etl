0a78fd69645b03fa7710e2a172bfe6168348d57f
Eddie Lin <elin@mozilla.com>
2019-10-02 14:51:24 +0800

    Use persistent function instead of temnp function.

--- udf_js/json_extract_events.sql
+++ udf_js/json_extract_events.sql
@@ -1,14 +1,12 @@
-CREATE TEMP FUNCTION
-  udf_js_json_extract_events (input STRING)
-
-  RETURNS ARRAY<STRUCT<
-  event_timestamp INT64,
+CREATE OR REPLACE FUNCTION
+  `{project}.{dataset}`.udf_js_json_extract_events (input STRING)
+  RETURNS ARRAY<STRUCT< event_timestamp INT64,
   event_category STRING,
   event_object STRING,
   event_method STRING,
   event_value STRING,
-  event_extra ARRAY<STRUCT<key STRING, value STRING>>
-  >>
+  event_extra ARRAY<STRUCT<key STRING,
+  value STRING>> >>
   LANGUAGE js AS """
     if (input == null) {
       return null;
@@ -17,15 +15,15 @@ CREATE TEMP FUNCTION
     var result = [];
     parsed.forEach(event => {
         var structured = {
-          "event_timestamp": event[0],
-          "event_category": event[1],
-          "event_method": event[2],
-          "event_object": event[3],
-          "event_value": event[4],
+          "event_timestamp": null == event[0] ? 0 : event[0],
+          "event_category": null == event[1] ? "" : event[1].toLowerCase(),
+          "event_method": null == event[2] ? "" : event[2].toLowerCase(),
+          "event_object": null == event[3] ? "" : event[3].toLowerCase(),
+          "event_value": null == event[4] ? "" : event[4].toLowerCase(),
           "event_extra": []
         }
         for (var key in event[5]) {
-          structured.event_extra.push({"key": key, "value": event[5][key]})
+          structured.event_extra.push({"key": key.toLowerCase(), "value": (null == event[5][key] ? "" : event[5][key].toLowerCase())})
         }
         result.push(structured)
     });
