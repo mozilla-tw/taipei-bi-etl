9210f1a9b1b0b0e347600c3e8ce91a36486ebb21
Eddie Lin <elin@mozilla.com>
2019-11-12 18:47:05 +0800

    Added mission sql.

---
+++ udf_js/cleanup_extra.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE FUNCTION `%s.%s`.udf_js_cleanup_extra (
+  build_id STRING, event_extra ARRAY<STRUCT<key STRING,value STRING>>
+)
+RETURNS ARRAY<STRUCT<key STRING,value STRING>>
+  LANGUAGE js
+AS """
+    if (event_extra == null) {
+      return null;
+    }
+    var result = [];
+    for (var i in event_extra) {
+      var k = event_extra[i]["key"].toLowerCase();
+      var v = (null == event_extra[i]["value"] ? "" : event_extra[i]["value"].toLowerCase());
+      if (k == "session_time" && parseInt(v) < 0) {
+        v = "0";
+      }
+      else if (k == "url_counts") {
+        v = (parseInt(v) + 1).toString();
+      }
+      result.push({"key": k, "value": v});
+    }
+    return result;
+""";
